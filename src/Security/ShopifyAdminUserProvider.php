b84e803501b4116b3d26571488e5e84cf6a2a78d
sergey.razumov <sergey.razumov@sharp-dev.net>
2020-02-18 14:33:29 +0300

    autowiring service and fix deprecated for symfony 4.4

--- src/Security/ShopifyAdminUserProvider.php
+++ src/Security/ShopifyAdminUserProvider.php
@@ -40,6 +40,6 @@ class ShopifyAdminUserProvider implements UserProviderInterface
 
     public function supportsClass($class)
     {
-        return $class instanceof ShopifyAdminUser;
+        return $class === ShopifyAdminUser::class;
     }
 }
